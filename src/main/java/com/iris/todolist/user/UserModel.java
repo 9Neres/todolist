9d1ee956d9c9ab83928724ae493c868326346543
9Neres <ricardoborgescontact@gmail.com>
2026-02-04 14:51:35 -0300

    feat: Create UserModel.java

---
+++ src/main/java/com/iris/todolist/user/UserModel.java
@@ -0,0 +1,26 @@
+package com.iris.todolist.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_user")
+public class UserModel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+    private String name;
+
+    @CreationTimestamp
+    private LocalDateTime CreatedAt;
+}
